485d8c8f0298c691d16a6d414047ea91c51fefb2
romeuic <casarottori@gmail.com>
2023-03-31 19:43:43 -0300

    converte componentes para styled

--- client/src/components/CourseList/CourseList.jsx
+++ client/src/components/CourseList/CourseList.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types'
 import CourseCard from '../CourseCard'
-import './CourseList.css'
+import { CourseListContainer } from './styles'
 
 function CourseList({ list }) {
   return (
-    <ul className="courseListContainer">
+    <CourseListContainer>
       {list.map((course) => (
         <li key={course.id}>
           <CourseCard
@@ -17,7 +17,7 @@ function CourseList({ list }) {
           />
         </li>
       ))}
-    </ul>
+    </CourseListContainer>
   )
 }
 
