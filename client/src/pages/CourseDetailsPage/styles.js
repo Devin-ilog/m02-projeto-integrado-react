c17e119875e1d28fff511c1fbed21a9abecb4051
romeuic <casarottori@gmail.com>
2023-03-30 21:11:40 -0300

    adiciona ex4 converte parcialmente paginas

--- client/src/pages/CourseDetailsPage/styles.js
+++ client/src/pages/CourseDetailsPage/styles.js
@@ -1,12 +1,14 @@
-.courseDetailsPageContainer {
+import styled from 'styled-components'
+
+export const CourseDetailsPageContainer = styled.div`
   flex-grow: 1;
   display: flex;
   justify-content: center;
   align-items: center;
   gap: var(--spacing-5);
-}
+`
 
-.courseDetailsCenterBox {
+export const CourseDetailsCenterBox = styled.div`
   width: 629px;
   max-width: 100%;
   display: flex;
@@ -18,4 +20,4 @@
   background: var(--white);
   box-shadow: 0 var(--spacing-2) var(--spacing-2) rgba(0, 0, 0, 0.25);
   border-radius: var(--border-radius-1);
-}
+`
